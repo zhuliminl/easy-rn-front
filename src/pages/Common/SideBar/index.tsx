08dba2bfe0bbdb7452591f0a5db2e82ae2fb3521
zhuliminl <zhuliminl@gmail.com>
2022-11-20 01:05:54 +0800

    jjjj

--- src/pages/Common/SideBar/index.tsx
+++ src/pages/Common/SideBar/index.tsx
@@ -1,6 +1,13 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+
 import React from 'react'
+import { jsx, css } from '@emotion/react'
+
 export default () => {
   return (
-    <div>SideBar</div>
+    <React.Fragment>
+      <div>SideBar</div>
+    </React.Fragment>
   )
 }
